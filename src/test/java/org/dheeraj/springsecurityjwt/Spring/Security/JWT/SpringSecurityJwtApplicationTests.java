67c13cdf038d3d3298017c0c65fd9070a4821293
Dheeraj kumar <kdheeraj.cse@gmail.com>
2021-11-11 00:11:41 +0530

    initial commit

---
+++ src/test/java/org/dheeraj/springsecurityjwt/Spring/Security/JWT/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dheeraj.springsecurityjwt.Spring.Security.JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
